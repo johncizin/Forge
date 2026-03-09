1dd02983cb71e221d4299cbf9fbd1e0f2785eb05
John Cizin <68138359+SoaringAbove@users.noreply.github.com>
2026-03-09 16:02:38 -0400

    initializiing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">Forge</h1>
+    </div>
+  )
+}
+
+export default App
